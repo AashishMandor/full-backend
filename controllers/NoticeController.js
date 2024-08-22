51ed324c04f4b7e6426699900c4d2b1bfcdecbf1
aashish mandor <aashishmandor@gmail.com>
2024-08-22 15:44:39 +0530

    fifth

--- controllers/NoticeController.js
+++ controllers/NoticeController.js
@@ -34,3 +34,13 @@ export const createnotice = async (req, res) => {
 //       res.status(500).json({ message: 'Something went wrong', error });
 //     }
 //   };
+
+
+export const getAllNotice = async (req, res) => {
+    try {
+      const notice = await Notice.findAll();
+      res.status(200).json(notice);
+    } catch (error) {
+      res.status(500).json({ message: 'Something went wrong', error });
+    }
+  };
--- routes/NoticeRoutes.js
+++ routes/NoticeRoutes.js
@@ -1,11 +1,15 @@
 
 import express from 'express'
-import { createnotice } from '../controllers/NoticeController.js'
+import {
+    createnotice,
+    getAllNotice,
+} from '../controllers/NoticeController.js'
 
 
 
 const router =express.Router()
 router.post('/',createnotice)
+router.get('/all',getAllNotice)
 
 
 export default router
